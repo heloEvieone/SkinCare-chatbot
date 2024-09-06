54379cc185d661da7cb52deda4ebc463831091d7
heloEvieone <mtkyaw04@gmail.com>
2024-09-06 21:25:53 +0630

    edit: SidebarRight

--- src/components/SidebarRight.js
+++ src/components/SidebarRight.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Box, Avatar, Button, List, ListItem, ListItemText } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 import './SidebarRight.css';
 
 const SidebarRight = () => {
@@ -8,31 +10,62 @@ const SidebarRight = () => {
       {/* User Info */}
       <div className='inner'>
       <Box className="user-info">
-        <Avatar sx={{ marginRight: '8px', backgroundColor: '#42a5f5' }}>R</Avatar>
         <Box>
           <strong>Rhyli James</strong>
         </Box>
+        <Avatar sx={{ marginLeft: '8px', backgroundColor: '#42a5f5' }}>R</Avatar>
       </Box>
+      </div>
+      <Divider sx={{ height:'1px', 
+                      backgroundColor: '#00000033', 
+                      marginBottom: '2px'}} 
+                className='divider'/>
 
-      {/* New Chat Button */}
-      <Button variant="contained" fullWidth className="new-chat-btn">
-        New Chat
-      </Button>
-
-      {/* Chat History */}
+    {/* New Chat */}
       <List component="nav">
-        <ListItem button>
-          <ListItemText primary="Hair Care" className="chat-history-item" />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary="Body Care" className="chat-history-item" />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary="Korean Products" className="chat-history-item" />
+      <ListItem button>
+        <div className='new-chat-btn'>
+          <ListItemText primary="New Chat" className="new-chat-item" />
+          <AddBoxIcon/>
+          </div>
         </ListItem>
       </List>
+
+      <div className='history-btn-gp'>
+      <Button variant="contained" fullWidth className="chat-history-btn"
+        sx={{ backgroundColor: '#E9EAFE',
+              color: '#000000',
+              boxShadow: '0px 4px 4px 0px #00000040',
+              borderRadius: '12px',
+              marginBottom: '20px'
+        }}
+      >
+       Hair Care
+      </Button>
+      <Button variant="contained" fullWidth className="chat-history-btn"
+        sx={{ backgroundColor: '#E9EAFE',
+              color: '#000000',
+              boxShadow: '0px 4px 4px 0px #00000040',
+              borderRadius: '12px',
+              marginBottom: '20px'
+        }}
+      >
+        Body Care
+      </Button>
+      <Button variant="contained" fullWidth className="chat-history-btn"
+        sx={{ backgroundColor: '#E9EAFE', 
+              color: '#000000',
+              boxShadow: '0px 4px 4px 0px #00000040',
+              borderRadius: '12px',
+              marginBottom: '20px'
+        }}
+      >
+        SkinCare
+      </Button>
       </div>
     </Box>
+    
+
    
   );
 };
